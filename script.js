75ee153ad34f408c6276c9a8a63d7e70a5600c8f
Syed Tanvir Hossain <tanvirjoy04@gmail.com>
2026-05-21 14:49:35 +0600

    create functions

---
+++ script.js
@@ -0,0 +1,17 @@
+//creating functions
+function add(a,b){
+    return a+b;
+}
+function sub(a,b){
+    return a-b;
+}
+function mul(a,b){
+    return a*b;
+}
+function div(a,b){
+    return a/b;
+}
+console.log(add(3,7));
+console.log(sub(5,3));
+console.log(mul(3,3));
+console.log(div(5,2));
